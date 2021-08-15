2ad5b9db1f5a973fbde3854ea521a1a07c9c77c1
jbkim <jbkim@DESKTOP-1248CM3>
2021-08-16 06:37:03 +0900

    검색 필터링, 조회 필터링 구현 (90%)
    고객 캠페인 디테일 AJAX를 통한 추가기능 구현 (50%)

--- src/main/java/kr/co/earthnus/user/camBoard/CamBoardController.java
+++ src/main/java/kr/co/earthnus/user/camBoard/CamBoardController.java
@@ -1,10 +1,14 @@
 package kr.co.earthnus.user.camBoard;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CamBoardController{	
@@ -16,8 +20,11 @@ public class CamBoardController{
 			@RequestParam(defaultValue = "6") String contentnum, camBoardBean bean, Model model) {
 		
 		String search = "%%";
+		String search_type = "CAMB_ENTIRE";
+		String orderBy = "CAMB_NUM";
+		String order = "DESC";
 		
-		camBoardService.getCamBoardList(search, arr, bean, pagenum, contentnum, model);
+		camBoardService.getBoardList(search, search_type, arr, orderBy, order, contentnum, pagenum, model);
 		
 		System.out.println("CamBoardController");
 		return "camBoard/camBoardList";
@@ -26,7 +33,7 @@ public class CamBoardController{
 	@RequestMapping("/camBoard/list/search")
 	public String searchCamBoardList(@RequestParam(defaultValue = "entire") String arr, @RequestParam(defaultValue = "1") String pagenum, 
 			@RequestParam(defaultValue = "6") String contentnum, @RequestParam( value = "search", required=false) String search ,
-			camBoardBean bean, Model model) {
+			@RequestParam(defaultValue = "desc") String order, camBoardBean bean, Model model) {
 		
 		System.out.println("컨트롤러 검색어 : " + search);
 		
@@ -35,8 +42,10 @@ public class CamBoardController{
 		}else if(search == null || search.equals("")) {
 			search = "%%";
 		}
+		String search_type = "CAMB_ENTIRE";
+		String orderBy = "CAMB_NUM";
 		
-		camBoardService.searchCamBoard(arr, search, bean, pagenum, contentnum, model);
+		camBoardService.getBoardList(search, search_type, arr, orderBy, order, contentnum, pagenum, model);
 		
 		System.out.println("CamBoardController");
 		return "camBoard/camBoardList";
@@ -49,5 +58,25 @@ public class CamBoardController{
 		model.addAttribute("camBoard", camBoardService.getCamBoard(contentnum));		
 		
 		return "camBoard/camBoardDetail";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/getList", method=RequestMethod.GET)
+	public List<camBoardBean> list(@RequestParam(defaultValue = "entire") String arr, @RequestParam(defaultValue = "1") String pagenum, 
+			@RequestParam(defaultValue = "6") String contentnum, @RequestParam( value = "search", required=false) String search ,
+			@RequestParam(defaultValue = "desc") String order, camBoardBean bean, Model model){
+		List<camBoardBean> CamBoardList = null;
+		
+		if(search != null) {
+			search = "%" + search + "%";
+		}else if(search == null || search.equals("")) {
+			search = "%%";
+		}
+		String search_type = "CAMB_ENTIRE";
+		String orderBy = "CAMB_NUM";
+		
+		CamBoardList = camBoardService.getList(search, search_type, arr, orderBy, order, contentnum, pagenum, CamBoardList, model);
+		
+		return CamBoardList;
 	}
 }
