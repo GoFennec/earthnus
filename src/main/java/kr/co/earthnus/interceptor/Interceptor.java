f09c7b6bb655b1bb5fa0cfdb78be3855fcc89f38
yoonbb <yoonzoz@naver.com>
2021-09-06 18:45:18 +0900

    merging

--- src/main/java/kr/co/earthnus/interceptor/Interceptor.java
+++ src/main/java/kr/co/earthnus/interceptor/Interceptor.java
@@ -11,26 +11,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class Interceptor extends HandlerInterceptorAdapter {
-	private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
+   private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
+   @Override
+   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+         throws Exception {
 
-		HttpSession session = request.getSession();
-		// AuthBean aBean= (AuthBean)session.getAttribute("aBean");
+      HttpSession session = request.getSession();
+      // AuthBean aBean= (AuthBean)session.getAttribute("aBean");
 
-		if (session.getAttribute("auth") == null) {
-			response.setContentType("text/html;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script>alert('로그인이 필요한 서비스입니다.\r\n" + 
-					"로그인 페이지로 이동합니다.'); location.href=\"/auth/login\"</script>");
-			out.close();
-			String referrer = request.getHeader("Referer");
-			request.getSession().setAttribute("url_prior_login", referrer);
-			// response.sendRedirect("/auth/login");
-			return false;
-		}
-		return true;
-	}
+      if (session.getAttribute("auth") == null) {
+         response.setContentType("text/html;charset=UTF-8");
+         PrintWriter out = response.getWriter();
+         out.println("<script>alert('로그인페이지로 이동합니다.'); location.href=\"/auth/login\"</script>");
+         out.close();
+         String referrer = request.getHeader("Referer");
+         request.getSession().setAttribute("url_prior_login", referrer);
+         // response.sendRedirect("/auth/login");
+         return false;
+      }
+      return true;
+   }
 }
