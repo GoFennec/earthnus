49cfe72b24856b2396a8a0ee117ffb1584e37c7b
yooingil <fkskfjei@gmail.com>
2021-09-14 18:27:34 +0900

    viewupdate like_update

--- src/main/java/kr/co/earthnus/user/cheBoard/CheBoardMybatis.java
+++ src/main/java/kr/co/earthnus/user/cheBoard/CheBoardMybatis.java
@@ -1,5 +1,6 @@
 package kr.co.earthnus.user.cheBoard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import kr.co.earthnus.user.pay.PayBean;
@@ -17,4 +18,14 @@ public interface CheBoardMybatis {
   void Comment_like(like_Bean likebean);
 
   void pay_comment_update(CheBoardBean BoardBean);
+
+void Comment_like_candle(like_Bean likebean);
+
+void like_countplus(like_Bean likebean);
+
+void like_countdelete(like_Bean likebean);
+
+int get_select_total_like(like_Bean likebean);
+
+ArrayList<Integer> get_like_num(String user_id);
 }
--- src/main/java/kr/co/earthnus/user/cheBoard/CheBoardService.java
+++ src/main/java/kr/co/earthnus/user/cheBoard/CheBoardService.java
@@ -1,5 +1,7 @@
 package kr.co.earthnus.user.cheBoard;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -14,55 +16,76 @@ public class CheBoardService {
   @Autowired
   private SqlSessionTemplate mybatis;
   
+  public List<CheBoardBean> selectAllComment(String startnum, String comment_step) {
+	    CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
+	    PageBean bean = new PageBean();
+	    List<CheBoardBean> CheBoardList = null;
+	    int start_num = Integer.parseInt(startnum);
+	    int step_num = Integer.parseInt(comment_step);
+	    bean.setComment_step(step_num);
+	    bean.setStartNum(start_num);
+	    
+	    CheBoardList = ChreBoardDAO.get_CheBoardList(bean);
+	    return CheBoardList;
+  }
+	  
+  public void donation_dname(Model model, String user_id) {
+	    CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
+	    List<PayBean> d_name = null;
+	    ArrayList<Integer> cheb_num = new ArrayList<Integer>();
+	    d_name = ChreBoardDAO.get_select_dname(user_id);
+	    int total_comment = ChreBoardDAO.get_total_comment();
+	    
+	    if(total_comment > 0) {
+	    	  cheb_num = ChreBoardDAO.get_like_num(user_id);
+	    }
+	    
+	    model.addAttribute("payCheck", d_name);
+	    model.addAttribute("total_comment", total_comment);
+	    
+	    if(cheb_num.size() != 0)
+	    	 model.addAttribute("like_check_num", cheb_num);
+ }
+  
+  
+  public void total_comment(Model model) {
+		CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
+		int total_comment = ChreBoardDAO.get_total_comment();
+		model.addAttribute("total_comment", total_comment);
+  }
+  
   public void deleteReply(int reply_id) {
     CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
     ChreBoardDAO.replyDelete(reply_id);
-   
   }
   
   public void replyInsert(CheBoardBean boardBean) {
     CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
     ChreBoardDAO.replyInsert(boardBean);
   }
-  
-  public List<CheBoardBean> selectAllComment(String startnum, String comment_step) {
-    CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
-    PageBean bean = new PageBean();
-    List<CheBoardBean> CheBoardList = null;
-    int start_num = Integer.parseInt(startnum);
-    int step_num = Integer.parseInt(comment_step);
-    bean.setComment_step(step_num);
-    bean.setStartNum(start_num);
-    
-    CheBoardList = ChreBoardDAO.get_CheBoardList(bean);
-    return CheBoardList;
-  }
-  
-  public void donation_dname(Model model, String user_id) {
-    CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
-    List<PayBean> d_name = null;
-    d_name = ChreBoardDAO.get_select_dname(user_id);
-    int total_comment = ChreBoardDAO.get_total_comment();
-    model.addAttribute("payCheck", d_name);
-    model.addAttribute("total_comment", total_comment);
-  }
-
- public void Comment_like(like_Bean likebean) {
-	 CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
-	 
-      ChreBoardDAO.Comment_like(likebean);
-     
-}
 
 public void pay_comment_update(CheBoardBean BoardBean) {
 	 CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
 	 ChreBoardDAO.pay_comment_update(BoardBean);
+  }
+
+
+public void Comment_like(like_Bean likebean) {
+	 CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
+	 ChreBoardDAO.like_countplus(likebean);
+     ChreBoardDAO.Comment_like(likebean);
+    
 }
 
-public void total_comment(Model model) {
+public void Comment_like_candle(like_Bean likebean) {
 	CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
-	int total_comment = ChreBoardDAO.get_total_comment();
-	model.addAttribute("total_comment", total_comment);
+	 ChreBoardDAO.Comment_like_candle(likebean);
+	 ChreBoardDAO.like_countdelete(likebean);
+}
+
+public int select_total_like(like_Bean likebean) {
+	CheBoardMybatis ChreBoardDAO = (CheBoardMybatis)this.mybatis.getMapper(CheBoardMybatis.class);
+	return ChreBoardDAO.get_select_total_like(likebean);
 }
 
 
