a8ba593eea2d9047bc37a6fa7aaa32cd08891e90
yoonbb <yoonzoz@naver.com>
2021-09-26 02:03:53 +0900

    merging

--- src/main/java/kr/co/earthnus/user/auth/KakaoAPI.java
+++ src/main/java/kr/co/earthnus/user/auth/KakaoAPI.java
@@ -27,11 +27,9 @@ public class KakaoAPI {
             URL url = new URL(reqURL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             
-            //    POST �슂泥��쓣 �쐞�빐 湲곕낯媛믪씠 false�씤 setDoOutput�쓣 true濡�
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             
-            //    POST �슂泥��뿉 �븘�슂濡� �슂援ы븯�뒗 �뙆�씪誘명꽣 �뒪�듃由쇱쓣 �넻�빐 �쟾�넚
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
@@ -41,11 +39,9 @@ public class KakaoAPI {
             bw.write(sb.toString());
             bw.flush();
             
-            //    寃곌낵 肄붾뱶媛� 200�씠�씪硫� �꽦怨�
             int responseCode = conn.getResponseCode();
             System.out.println("responseCode : " + responseCode);
  
-            //    �슂泥��쓣 �넻�빐 �뼸�� JSON���엯�쓽 Response 硫붿꽭吏� �씫�뼱�삤湲�
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
             String result = "";
@@ -55,7 +51,6 @@ public class KakaoAPI {
             }
             System.out.println("response body : " + result);
             
-            //    Gson �씪�씠釉뚮윭由ъ뿉 �룷�븿�맂 �겢�옒�뒪濡� JSON�뙆�떛 媛앹껜 �깮�꽦
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
             
@@ -77,7 +72,6 @@ public class KakaoAPI {
     
   public HashMap<String, Object> getUserInfo (String access_Token) {
         
-        //    �슂泥��븯�뒗 �겢�씪�씠�뼵�듃留덈떎 媛�吏� �젙蹂닿� �떎瑜� �닔 �엳湲곗뿉 HashMap���엯�쑝濡� �꽑�뼵
         HashMap<String, Object> userInfo = new HashMap<String, Object>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
         try {
@@ -85,7 +79,6 @@ public class KakaoAPI {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             
-            //    �슂泥��뿉 �븘�슂�븳 Header�뿉 �룷�븿�맆 �궡�슜
             conn.setRequestProperty("Authorization", "Bearer " + access_Token);
             
             int responseCode = conn.getResponseCode();
