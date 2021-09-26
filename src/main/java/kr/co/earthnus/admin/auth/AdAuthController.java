96d0b59cd4cac77c84557df017d37fc9302d1ada
YukyungKang93 <87309367+YukyungKang93@users.noreply.github.com>
2021-09-26 13:33:56 +0900

    0926

--- src/main/java/kr/co/earthnus/admin/auth/AdAuthController.java
+++ src/main/java/kr/co/earthnus/admin/auth/AdAuthController.java
@@ -75,6 +75,7 @@ public class AdAuthController {
 			session.setAttribute("adauth", aBean);
 			return "redirect:/adIndex";
 		} else {
+			response.setCharacterEncoding("UTF-8");
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			out.println("<script>alert('아이디 또는 비밀번호가 일치하지 않습니다.');location.href=\"/auth/adLogin\"</script>");
